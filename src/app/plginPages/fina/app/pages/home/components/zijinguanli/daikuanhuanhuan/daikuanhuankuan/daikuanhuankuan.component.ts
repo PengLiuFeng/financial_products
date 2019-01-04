6b43d0a20e622a9d9d5e85b9a13b27f20df88ed8
pengliufeng <pengliufeng@gcstgroup.com>
2019-01-04 09:16:10 +0800

    贷款还款

--- src/app/plginPages/fina/app/pages/home/components/zijinguanli/daikuanhuanhuan/daikuanhuankuan/daikuanhuankuan.component.ts
+++ src/app/plginPages/fina/app/pages/home/components/zijinguanli/daikuanhuanhuan/daikuanhuankuan/daikuanhuankuan.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit ,ViewChildren,QueryList } from '@angular/core';
-
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
+import { ParamsService } from './../../../../../../params.service'
+import { ModalDirective } from 'ng-uikit-pro-standard';
 @Component({
   selector: 'app-daikuanhuankuan',
   templateUrl: './daikuanhuankuan.component.html',
@@ -7,30 +8,33 @@ import { Component, OnInit ,ViewChildren,QueryList } from '@angular/core';
 })
 export class DaikuanhuankuanComponent implements OnInit {
   @ViewChildren('pages') pages: QueryList<any>;
-  constructor() { }
-//查询条件值绑定
-client: any = {
-  cuName: '',
-  cuNo:'',
-  busType:'',       //业务类型
-  contNo:'',        //合同编号
-  loanSta:'',       //合同状态
-  startTimestamp: '',   //合同其实日期
-  dueTimestamp: '',         //合同到期日
-}
-//下拉框
-busTypes=[];      //业务类型
-loanStas=[];      //合同状态
-title = [//当前页面位置
-  "融资管理",
-  "资金管理",
-  "贷款还款"
-]
-theTotalNumberOfEntries = 0;
+  _http:any;
+  constructor(public params:ParamsService) { 
+    this._http=params._http;
+  }
+  //查询条件值绑定
+  client= {
+    cuName: '',
+    cuNo: '',
+    busType: '',       //业务类型
+    contNo: '',        //合同编号
+    loanSta: '',       //合同状态
+    startTimestamp: '',   //合同其实日期
+    dueTimestamp: '',         //合同到期日
+  }
+  //下拉框
+  busTypes = [];      //业务类型
+  loanStas = [];      //合同状态
+  title = [//当前页面位置
+    "融资管理",
+    "资金管理",
+    "贷款还款"
+  ]
+  theTotalNumberOfEntries = 0;
   tableData = []
   ngOnInit() {
   }
-  requestTableData(){};           //请求新的数据
+  requestTableData() { };           //请求新的数据
   //分页数据
   pageTable = [];
   activePage = 1;

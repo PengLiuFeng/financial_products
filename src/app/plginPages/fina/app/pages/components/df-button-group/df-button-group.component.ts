cc4a7a591013a13cbead1099f238b1664b2182c1
董飞飞 <dong.feifei@gcstgroup.com>
2019-01-09 18:41:33 +0800

    dev

---
+++ src/app/plginPages/fina/app/pages/components/df-button-group/df-button-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'df-button-group',
+  templateUrl: './df-button-group.component.html',
+  styleUrls: ['./df-button-group.component.scss']
+})
+export class DfButtonGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
